3ac78008c0ac8e43695546a81f03858c56ee1958
liuweishi <1434967721@qq.com>
2020-05-22 22:41:46 +0800

    更新代码

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,19 @@
+const {seller,goods,ratings } =require("./data/data.json")
 module.exports={
   lintOnSave:false,
   devServer:{
-      open:true
+      open:true,
+      before: function(app) {
+        //app 就是我们express的核心对象  可以用来注册一个后台路由
+        app.get('/api/seller', function(req, res) {
+            res.json({seller});
+        });
+        app.get('/api/goods', function(req, res) {
+            res.json({goods});
+        });
+        app.get('/api/ratings', function(req, res) {
+            res.json({ratings});
+        });
+    }
   }
 }
